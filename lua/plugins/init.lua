858b754cbb47b1e50d5f913e6413d4b6ba33852d
rock-tran <rock.tran@manadr.com>
2022-10-07 17:30:27 +0700

    Update a bit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -203,10 +203,18 @@ local plugins = {
       require("core.utils").load_mappings "whichkey"
     end,
   },
+
   ["neoclide/coc.nvim"] = {
-    branch = "release"
+    branch = "release",
   },
 }
+
+local PythonPath = vim.env.PYENV_VIRTUAL_ENV
+PythonPath = PythonPath .. "/bin/python"
+vim.fn['coc#config']('python', {
+  pythonPath = PythonPath
+})
+
 
 -- Load all plugins
 local present, packer = pcall(require, "packer")
