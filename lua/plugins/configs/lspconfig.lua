42cf7feef5b7fc255762e18ca30c10bb880cdc6b
rock-tran <rock.tran@manadr.com>
2024-03-06 10:12:11 +0700

    fix(lua_ls) : configure it in custom configs

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -69,7 +69,7 @@ M.defaults = function()
   -- require("lspconfig").lua_ls.setup {
   --   on_attach = M.on_attach,
   --   capabilities = M.capabilities,
-  --
+  --   on_init = M.on_init,
   --   settings = {
   --     Lua = {
   --       diagnostics = {
@@ -88,29 +88,6 @@ M.defaults = function()
   --     },
   --   },
   -- }
-  require("lspconfig").lua_ls.setup {
-    on_attach = M.on_attach,
-    capabilities = M.capabilities,
-    on_init = M.on_init,
-
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = {
-            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-            [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-            [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
-            [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
-          },
-          maxPreload = 100000,
-          preloadFileSize = 10000,
-        },
-      },
-    },
-  }
 end
 
 return M
