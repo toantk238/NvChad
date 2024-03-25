6a8bbaed1cb66652c8b9d542ba38ac8d4f701f6d
rock-tran <rock.tran@manadr.com>
2024-03-25 17:07:39 +0700

    Remove useless mappings (to use tmux-navigation)

--- lua/nvchad/mappings.lua
+++ lua/nvchad/mappings.lua
@@ -2,17 +2,8 @@ local map = vim.keymap.set
 
 map("i", "<C-b>", "<ESC>^i", { desc = "Move Beginning of line" })
 map("i", "<C-e>", "<End>", { desc = "Move End of line" })
-map("i", "<C-h>", "<Left>", { desc = "Move Left" })
-map("i", "<C-l>", "<Right>", { desc = "Move Right" })
-map("i", "<C-j>", "<Down>", { desc = "Move Down" })
-map("i", "<C-k>", "<Up>", { desc = "Move Up" })
 
 map("n", "<Esc>", "<cmd>noh<CR>", { desc = "General Clear highlights" })
-
-map("n", "<C-h>", "<C-w>h", { desc = "Switch Window left" })
-map("n", "<C-l>", "<C-w>l", { desc = "Switch Window right" })
-map("n", "<C-j>", "<C-w>j", { desc = "Switch Window down" })
-map("n", "<C-k>", "<C-w>k", { desc = "Switch Window up" })
 
 map("n", "<C-s>", "<cmd>w<CR>", { desc = "File Save" })
 map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "File Copy whole" })
