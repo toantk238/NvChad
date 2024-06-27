783b97d5953a3b2ffcad353332c2b3bba8064326
rock-tran <rock.tran@manadr.com>
2024-06-27 09:44:40 +0700

    Update mappings

--- lua/nvchad/configs/gitsigns.lua
+++ lua/nvchad/configs/gitsigns.lua
@@ -17,9 +17,9 @@ local options = {
 
     local map = vim.keymap.set
 
-    map("n", "<leader>rh", gs.reset_hunk, opts "Reset Hunk")
-    map("n", "<leader>ph", gs.preview_hunk, opts "Preview Hunk")
-    map("n", "<leader>gb", gs.blame_line, opts "Blame Line")
+    -- map("n", "<leader>rh", gs.reset_hunk, opts "Reset Hunk")
+    -- map("n", "<leader>ph", gs.preview_hunk, opts "Preview Hunk")
+    -- map("n", "<leader>gb", gs.blame_line, opts "Blame Line")
   end,
 }
 
