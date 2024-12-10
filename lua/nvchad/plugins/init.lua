4f4d2d7ccb4de2a6cef348f3d4d9b1e9070f2185
rock-tran <rock.tran@manadr.com>
2024-12-10 15:39:26 +0700

    Update Luasnip

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -109,6 +109,9 @@ return {
         -- snippet plugin
         "L3MON4D3/LuaSnip",
         dependencies = "rafamadriz/friendly-snippets",
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
         opts = { history = true, updateevents = "TextChanged,TextChangedI" },
         config = function(_, opts)
           require("luasnip").config.set_config(opts)
