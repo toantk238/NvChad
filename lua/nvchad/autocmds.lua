6ae382ed57f4a28364a284df54b0598ecf36fe06
rock-tran <rock.tran@manadr.com>
2026-01-27 14:30:00 +0700

    (fix): IDK how "lazy.core.config" can load the opts of nvim-treesitter

--- lua/nvchad/autocmds.lua
+++ lua/nvchad/autocmds.lua
@@ -36,7 +36,8 @@ vim.api.nvim_create_autocmd("FileType", {
 local create_cmd = vim.api.nvim_create_user_command
 
 create_cmd("TSInstallAll", function()
-  local spec = require("lazy.core.config").plugins["nvim-treesitter"]
-  local opts = type(spec.opts) == "table" and spec.opts or {}
+  -- local spec = require("lazy.core.config").plugins["nvim-treesitter"]
+  local opts = require("nvchad.configs.treesitter")
+  -- local opts = type(spec.opts) == "table" and spec.opts or {}
   require("nvim-treesitter").install(opts.ensure_installed)
 end, {})
